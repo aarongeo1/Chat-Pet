abb637e697af514348bcf0cfcbf63d073ad033e8
AARON <abinoy@ualberta.ca>
2022-12-26 17:54:47 -0600

    lines

--- pet.py
+++ pet.py
@@ -3,3 +3,54 @@ import pyautogui
 import random
 
 win = tkinter.Tk()
+
+idle = [tkinter.PhotoImage(file = r"C:\Users\Aaron Geo Binoy\Virtual-Pet\src\assets\Pochita_Idle_Animated_1.gif",format = 'gif -index %i' %(i)) for i in range(31)]
+
+
+x = 1400
+cycle = 0
+check = 1
+idle_num =[1,2,3,4]
+sleep_num = [10,11,12,13,15]
+walk_left = [6,7]
+walk_right = [8,9]
+event_number = random.randrange(1,3,1)
+
+
+
+
+
+
+def update(cycle,check,event_number,x):
+ #idle
+ if check ==0:
+  frame = idle[cycle]
+  cycle ,event_number = work(cycle,idle,event_number,1,9)
+  
+  win.geometry('100x100+'+str(1400)+'+1050')
+  label.configure(image=frame)
+  win.after(1,event,cycle,check,event_number,x)
+
+def event(cycle,check,event_number,x):
+ if event_number in idle_num:
+  check = 0
+  print('idle')
+  win.after(400,update,cycle,check,event_number,x)
+
+def work(cycle,frames,event_number,first_num,last_num):
+ if cycle < len(frames) -1:
+  cycle+=1
+ else:
+  cycle = 0
+  event_number = random.randrange(first_num,last_num+1,1)
+ return cycle,event_number
+
+win.config(highlightbackground='white')
+win.overrideredirect(True)
+win.wm_attributes('-transparentcolor','white')
+
+label = tkinter.Label(win,bd=0,bg='white')
+label.pack()
+
+win.after(1,update,cycle,check,event_number,x)
+win.mainloop()
