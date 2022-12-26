53e6729c54cef1ae49410743f58fbe6091eed45f
AARON <90492963+aarongeo1@users.noreply.github.com>
2022-12-26 17:56:23 -0600

    Update pet.py

--- pet.py
+++ pet.py
@@ -6,15 +6,8 @@ win = tkinter.Tk()
 
 idle = [tkinter.PhotoImage(file = r"C:\Users\Aaron Geo Binoy\Virtual-Pet\src\assets\Pochita_Idle_Animated_1.gif",format = 'gif -index %i' %(i)) for i in range(31)]
 
-
-x = 1400
 cycle = 0
 check = 1
-
-
-
-
-
 
 def update(cycle,check,event_number,x):
  #idle
