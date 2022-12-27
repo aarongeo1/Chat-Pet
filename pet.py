038b8cab44c9531a9401ff8c7728a88a5487a261
AARON <90492963+aarongeo1@users.noreply.github.com>
2022-12-26 18:15:37 -0600

    Update pet.py

--- pet.py
+++ pet.py
@@ -7,12 +7,20 @@ win = tkinter.Tk()
 idle = [tkinter.PhotoImage(file = r"C:\Users\Aaron Geo Binoy\Virtual-Pet\src\assets\Pochita_Idle_Animated_1.gif",format = 'gif -index %i' %(i)) for i in range(31)]
 
 
+x = 1400
+cycle = 0
+check = 1
+
 win.config(highlightbackground='white')
+label = tkinter.Label(win,bd=0,bg='white')
 win.overrideredirect(True)
 win.wm_attributes('-transparentcolor','white')
-
-label = tkinter.Label(win,bd=0,bg='white')
 label.pack()
 
-win.after(1,update,cycle,check,event_number,x)
+frame = idle[cycle]
+win.title('Hello Python')
+win.geometry('100x100+'+"1400"+'+925')
+label.configure(image=frame)
+
 win.mainloop()
+
