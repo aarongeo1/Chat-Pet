d9615a4e529e0dd615cd06a92d5b691ad1cae851
AARON <90492963+aarongeo1@users.noreply.github.com>
2023-01-15 07:57:15 -0600

    moves left now

--- pet.py
+++ pet.py
@@ -5,8 +5,8 @@ import sys
 from ctypes import windll, wintypes, byref
 
 SPI_GETWORKAREA = 0x0030
-    
-# This var will receive the result to SystemParametersInfoW  
+
+# This var will receive the result to SystemParametersInfoW
 desktopWorkingArea = wintypes.RECT()
 
 _ = windll.user32.SystemParametersInfoW(SPI_GETWORKAREA,0,byref(desktopWorkingArea),0)
@@ -22,10 +22,12 @@ win = tkinter.Tk()
 
 # importing the idle gif
 idle = [tkinter.PhotoImage(file = r"C:\Users\Aaron Geo Binoy\Virtual-Pet\src\assets\Pochita_Idle_Animated_1.gif",format = 'gif -index %i' %(i)) for i in range(31)]
+walk_left = [tkinter.PhotoImage(file = r"C:\Users\Aaron Geo Binoy\Virtual-Pet\src\assets\Pochita_walking_2.gif",format = 'gif -index %i' %(i)) for i in range(11)]
 
 cycle = 0
-check = 1 # 1 == idle
-
+check = 0 # 0 == idle
+x = 1400
+time = 2400
 # making the background transparent
 win.config(highlightbackground='white')
 label = tkinter.Label(win,bd=0,bg='white')
@@ -35,6 +37,7 @@ win.wm_attributes('-transparentcolor','white')
 label.pack()
 
 # to iterate among the index of the array of frames
+
 def run(cycle,array):
     if cycle < len(array)-1:
         cycle += 1
@@ -43,21 +46,43 @@ def run(cycle,array):
     return cycle
 
 # function to change frames
-def change(cycle,check):
-    if check == 1:
+def change(cycle,check,time):
+    time -= 75
+    if time == 0:
+        check = random.randint(0,1)
+        time = 2400
+
+    if check == 0:
         frame = idle[cycle]
         cycle = run(cycle,idle)
-    win.geometry('100x100+'+"1000"+'+{}'.format(bottom - (y-bottom) - 50))
-    label.configure(image=frame)
-    win.after(75,change,cycle,check)
+        #check +=1
+        win.geometry('100x100+' + "1000" + '+{}'.format(bottom - (y - bottom) - 50))
+        label.configure(image=frame)
+        win.after(75, change, cycle, check,time)
 
-# to start the loop 
-win.after(1,change,cycle,check)
+
+    elif check == 1:
+        global x
+        frame = walk_left[cycle]
+        cycle = run(cycle,walk_left)
+        win.geometry('100x100+' + "1000" + '+{}'.format(bottom - (y - bottom) - 50))
+        label.configure(image=frame)
+        win.after(75, change, cycle, check,time)
+        x-=3
+
+    #win.geometry('100x100+' + "1000" + '+{}'.format(bottom - (y - bottom) - 50))
+    # win.geometry('100x100+'+str(x)+'+1050')
+    # label.configure(image=frame)
+    
+
+
+# to start the loop
+check = random.randint(0,1)
+win.after(1,change,cycle,check,time)
 
 # to render the pet above all windows
 win.lift()
-win.attributes('-topmost',True)
-win.after_idle(win.attributes,'-topmost',True)
+win.attributes('-topmost',False)
+win.after_idle(win.attributes,'-topmost',False)
 
 win.mainloop()
-
